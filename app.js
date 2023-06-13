e096021e4d4dbde99141715f986fc0bafdb883e3
Draxster <118171977+Draxster@users.noreply.github.com>
2023-06-13 15:25:40 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// const button = document.getElementById('button-1');
+// const text = document.getElementById('text');
+
+// button.addEventListener('click', () => {
+//   button.style.opacity = 0;
+
+//   text.style.display = 'flex'; 
+//   text.style.animation = "slider 1s ease-in-out";
+// });
